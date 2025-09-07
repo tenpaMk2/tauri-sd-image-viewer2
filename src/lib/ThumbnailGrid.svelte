ebfc8595ab63191d2e903775919a649f78f38fe4
tenpamk2 <wavyroom@gmail.com>
2025-09-07 16:40:12 +0900

    :lipstick: サムネイルサイズがウィンドウサイズに対して動的に決まるように。

--- src/lib/ThumbnailGrid.svelte
+++ src/lib/ThumbnailGrid.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <!-- サムネイルグリッド -->
-<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+<div class="grid grid-cols-[repeat(auto-fill,minmax(min(theme(spacing.48),30vw),1fr))] gap-4">
 	{#each imageFiles as imagePath (imagePath)}
 		<ImageThumbnail {imagePath} />
 	{/each}

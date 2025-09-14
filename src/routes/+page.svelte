45c22b35e3a4dc992cc68082e4cff01d72b72931
tenpamk2 <wavyroom@gmail.com>
2025-09-14 14:36:55 +0900

    :zap: Welcomeページ整備

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { asset } from '$app/paths';
 	import { dialogService } from '$lib/services/dialog';
+	import Icon from '@iconify/svelte';
 	import { app } from '@tauri-apps/api';
 
 	// ファイル選択ハンドラー
@@ -19,24 +21,33 @@
 		}
 	};
 
-	const version = app.getVersion();
+	const versionPromise = app.getVersion();
 </script>
 
 <svelte:head>
 	<title>Image Viewer</title>
 </svelte:head>
 
-<div class="flex h-full flex-col items-center justify-center bg-base-100 text-center">
-	<div class="mb-4 text-6xl">📁TODO: アイコン表示</div>
-	<h2 class="mb-2 text-2xl font-bold">Tauri SD Image Viewer</h2>
-	{#await version}
-		<p class="mb-2 text-sm text-base-content/50">v{version}</p>
-	{/await}
-	<p class="mb-6 text-base-content/70">Please select a file or folder</p>
-	<div class="flex flex-col gap-4">
-		<div class="flex gap-4">
-			<button class="btn btn-lg btn-primary" onclick={openFileDialog}> Open Image File </button>
-			<button class="btn btn-lg btn-secondary" onclick={openDirectoryDialog}> Open Folder </button>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-row">
+		<img src={asset('/icon.png')} alt="App Icon" class=" w-56 shadow-lg" />
+		<div>
+			{#await versionPromise}
+				<h1 class="text-4xl font-bold">Tauri SD Image Viewer</h1>
+			{:then version}
+				<h1 class="text-4xl font-bold">Tauri SD Image Viewer v{version}</h1>
+			{/await}
+			<p class="my-6 text-base-content/70">Please select a file or folder</p>
+			<div class="flex gap-4">
+				<button class="btn btn-lg btn-primary" onclick={openFileDialog}>
+					<Icon icon="lucide:image-plus" class="mr-2 h-5 w-5" />
+					Open Image File
+				</button>
+				<button class="btn btn-lg btn-secondary" onclick={openDirectoryDialog}>
+					<Icon icon="lucide:folder" class="mr-2 h-5 w-5" />
+					Open Folder
+				</button>
+			</div>
 		</div>
 	</div>
 </div>
